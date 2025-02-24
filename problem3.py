00f3a184678bf0d0e01e0f68325778900c6d390c
Anya Lilly <137827671+anyalilly@users.noreply.github.com>
2025-02-24 23:37:58 +0000

    ucase strategy

--- problem3.py
+++ problem3.py
@@ -1,4 +1,24 @@
 # Anya prob 3
     """
-    hi
-    ""
+    Understanding:
+    take any number and tell the user whether it's even or odd
+    Input: Number
+    Output: Whether it's even or odd, true or false
+    Edge case: zero, irrational numbers, fractions
+    solution: limit to only integers, and give error message when input isn't an integer
+    """
+    """
+    - integer input
+    - mod % 2
+    - print
+    - boolean variable
+    """
+    """"
+    psuedocode:
+    ask user for integer
+    check if integer can be divided by two
+    if true then print true
+    else print false
+    """
+integer = int(input('Enter any whole number: '))
+
